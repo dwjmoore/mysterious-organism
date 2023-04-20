cae19b852b33d8cca8a41f8e079dbe5e931cb7f0
David Moore <dwjmoore@gmail.com>
2023-04-20 15:23:50 +0000

    created compare DNA method

--- index.js
+++ index.js
@@ -23,5 +23,21 @@ const pAequorFactory = (specimenNum, dna) => {
 			}
 			this.dna[randomIndex] = mutation;
 		},
+		compareDNA(otherPAequor) {
+			let commonDNACounter = 0;
+			for (let i = 0; i < this.dna.length; i++) {
+				if (this.dna[i] === otherPAequor.dna[i]) {
+					commonDNACounter++;
+				}
+			}
+			const percentCommonDNA = commonDNACounter / this.dna.length;
+			console.log(`Specimen #${this.specimenNum} and specimen #${otherPAequor.specimenNum} have ${Math.round(percentCommonDNA * 100)}% DNA in common.`);
+		}
 	};
 };
+
+const speciman1 = pAequorFactory(1, mockUpStrand());
+const speciman2 = pAequorFactory(2, mockUpStrand());
+console.log(speciman1.dna);
+console.log(speciman2.dna);
+speciman1.compareDNA(speciman2);
